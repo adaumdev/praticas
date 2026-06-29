8870708c732119519c56fbbab169eb9f018b5c68
Daii <dai@dai.com>
2026-06-29 14:05:39 -0300

    Adição de texto no lvl.

--- code/level.py
+++ code/level.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
 import pygame
 
+from pygame import Surface, Rect
+from pygame.font import Font
 from code.entity import Entity
 from code.entityFactory import EntityFactory
+from code.Const import COLOR_WHITE, WIN_HEIGHT
 
 class Level:
     def __init__(self, window, name, game_mode):
+        self.timeout = 1500
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('lvl1-'))
 
-    def run(self, ):
+    def run(self):
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(30)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT():
+                    pygame.quit()
+                    sys.exit()
+                
+            # printed text
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
         pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
