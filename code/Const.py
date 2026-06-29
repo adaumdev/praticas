eab43f585dfadfc28451e6847868c14ab2131386
Daii <dai@dai.com>
2026-06-29 03:07:00 -0300

    Adição do Menu Principal

---
+++ code/Const.py
@@ -0,0 +1,12 @@
+# C
+COLOR_L_BLUE = (102, 130, 219)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('START',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 1280
+WIN_HEIGHT = 720
